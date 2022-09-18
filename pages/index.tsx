ba1494ab6a38cbfe37258e9df22fb801053ae952
Juan Lopez <espinosalopezjuanramon@gmail.com>
2022-09-18 16:41:03 -0700

    Setup typescript

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Head from "next/head";
+
+const Home = () => {
+  return (
+    <>
+      <Head>
+        <title>Juan Lopez</title>
+      </Head>
+      Hello world
+    </>
+  );
+};
+
+export default Home;
