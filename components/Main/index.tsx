da1132f3745338e966bf8d64b2f3e707f023d4fb
Juan Lopez <espinosalopezjuanramon@gmail.com>
2023-08-06 00:56:14 -0700

    Add react three fiber

--- components/Main/index.tsx
+++ components/Main/index.tsx
@@ -1,8 +1,23 @@
 import React, { PropsWithChildren } from "react";
 import styles from "./styles.module.scss";
+import { Canvas } from "@react-three/fiber";
 
 const Main = ({ children }: PropsWithChildren) => {
-  return <main className={styles.main}>{children}</main>;
+  return (
+    <main className={styles.main}>
+      <div className={styles.main__canvas}>
+        <Canvas>
+          <ambientLight intensity={0.1} />
+          <directionalLight color="red" position={[0, 0, 5]} />
+          <mesh>
+            <boxGeometry />
+            <meshStandardMaterial />
+          </mesh>
+        </Canvas>
+      </div>
+      <div className={styles.main__content}>{children}</div>
+    </main>
+  );
 };
 
 export default Main;
--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withTM = require("next-transpile-modules")(["three"]);
+
 const nextConfig = {
   reactStrictMode: true,
-  compiler: {
-    styledComponents: true,
-  },
 };
 
-module.exports = nextConfig;
+module.exports = withTM(nextConfig);
