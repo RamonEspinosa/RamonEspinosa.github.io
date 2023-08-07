f8125c9c344bbd5eab64084f52a8312733c00ff9
Juan Lopez <espinosalopezjuanramon@gmail.com>
2023-08-07 11:34:47 -0700

    Back to framer motion due to LCP metrics

--- components/Contact/index.tsx
+++ components/Contact/index.tsx
@@ -1,8 +1,7 @@
 import { Github, Mail, Phone, Resume } from "components/Icons";
 import Link from "next/link";
 import React from "react";
-import { animated, useSpring } from "@react-spring/web";
-import { useGesture } from "@use-gesture/react";
+import { m, LazyMotion, domAnimation } from "framer-motion";
 import styles from "./styles.module.scss";
 
 const links = [
@@ -34,47 +33,24 @@ const links = [
 
 const Contact = () => {
   return (
-    <ul className={styles.contact__list}>
-      {links.map(({ Icon, label, to, props }) => {
-        // eslint-disable-next-line react-hooks/rules-of-hooks
-        const [{ scale }, api] = useSpring(
-          () => ({
-            scale: 1,
-            config: { tension: 400, friction: 15 },
-          }),
-          []
-        );
-
-        const handleHover = (hovering) =>
-          api({
-            scale: hovering ? 1.2 : 1,
-          });
-
-        // eslint-disable-next-line react-hooks/rules-of-hooks
-        const bind = useGesture({
-          onHover: ({ hovering }) => handleHover(hovering),
-          onFocus: ({}) => handleHover(true),
-          onBlur: () => handleHover(false),
-        });
-
-        return (
+    <LazyMotion features={domAnimation}>
+      <ul className={styles.contact__list}>
+        {links.map(({ Icon, label, to, props }) => (
           <li className={styles.contact__element} key={label}>
             <Link href={to} passHref {...props}>
-              <animated.a
+              <m.a
                 className={styles.contact__link}
+                whileHover={{ scale: 1.2 }}
                 target="_blank"
-                // whileHover={{ scale: 1.2 }}
-                // transition={{ type: "spring", stiffness: 400, damping: 10 }}
-                style={{ scale }}
-                {...bind()}
+                transition={{ type: "spring", stiffness: 400, damping: 10 }}
               >
                 <Icon aria-label={label} />
-              </animated.a>
+              </m.a>
             </Link>
           </li>
-        );
-      })}
-    </ul>
+        ))}
+      </ul>
+    </LazyMotion>
   );
 };
 
