8804792589ebf9fcd599962103916ad764de1725
Juan Lopez <espinosalopezjuanramon@gmail.com>
2023-08-07 11:25:41 -0700

    Remove 3d background, replace framer with react-spring

--- components/Background/index.tsx
+++ components/Background/index.tsx
@@ -1,20 +1,8 @@
-import { Canvas } from "@react-three/fiber";
-import Cloud from "components/Cloud";
-import CustomCameraControls from "components/CustomCameraControls";
 import React from "react";
 import styles from "./styles.module.scss";
 
 const Background = () => {
-  return (
-    <div className={styles.background__canvas}>
-      <Canvas>
-        <color attach="background" args={["black"]} />
-        <CustomCameraControls />
-        <ambientLight args={[0xffffff]} />
-        <Cloud />
-      </Canvas>
-    </div>
-  );
+  return <div className={styles.background}></div>;
 };
 
 export default Background;
--- components/Contact/index.tsx
+++ components/Contact/index.tsx
@@ -1,7 +1,8 @@
 import { Github, Mail, Phone, Resume } from "components/Icons";
 import Link from "next/link";
 import React from "react";
-import { m, LazyMotion, domAnimation } from "framer-motion";
+import { animated, useSpring } from "@react-spring/web";
+import { useGesture } from "@use-gesture/react";
 import styles from "./styles.module.scss";
 
 const links = [
@@ -33,24 +34,47 @@ const links = [
 
 const Contact = () => {
   return (
-    <LazyMotion features={domAnimation}>
-      <ul className={styles.contact__list}>
-        {links.map(({ Icon, label, to, props }) => (
+    <ul className={styles.contact__list}>
+      {links.map(({ Icon, label, to, props }) => {
+        // eslint-disable-next-line react-hooks/rules-of-hooks
+        const [{ scale }, api] = useSpring(
+          () => ({
+            scale: 1,
+            config: { tension: 400, friction: 15 },
+          }),
+          []
+        );
+
+        const handleHover = (hovering) =>
+          api({
+            scale: hovering ? 1.2 : 1,
+          });
+
+        // eslint-disable-next-line react-hooks/rules-of-hooks
+        const bind = useGesture({
+          onHover: ({ hovering }) => handleHover(hovering),
+          onFocus: ({}) => handleHover(true),
+          onBlur: () => handleHover(false),
+        });
+
+        return (
           <li className={styles.contact__element} key={label}>
             <Link href={to} passHref {...props}>
-              <m.a
+              <animated.a
                 className={styles.contact__link}
-                whileHover={{ scale: 1.2 }}
                 target="_blank"
-                transition={{ type: "spring", stiffness: 400, damping: 10 }}
+                // whileHover={{ scale: 1.2 }}
+                // transition={{ type: "spring", stiffness: 400, damping: 10 }}
+                style={{ scale }}
+                {...bind()}
               >
                 <Icon aria-label={label} />
-              </m.a>
+              </animated.a>
             </Link>
           </li>
-        ))}
-      </ul>
-    </LazyMotion>
+        );
+      })}
+    </ul>
   );
 };
 
