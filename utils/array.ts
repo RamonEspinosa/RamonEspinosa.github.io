e295c9d90996a5261fe8497e9d4c10b470912584
Juan Lopez <espinosalopezjuanramon@gmail.com>
2023-08-07 20:14:54 -0700

    Remove unused code

--- utils/array.ts
+++
@@ -1,8 +0,0 @@
-export function getRandomElement<T>(array: T[]): T | undefined {
-  if (array.length === 0) {
-    return undefined; // Return undefined if the array is empty
-  }
-
-  const randomIndex = Math.floor(Math.random() * array.length);
-  return array[randomIndex];
-}
