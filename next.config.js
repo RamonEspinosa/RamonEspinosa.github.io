96d2769e55bdd6b9cf16b607fbfe26a4bb1bdc72
Juan Lopez <espinosalopezjuanramon@gmail.com>
2023-08-08 22:38:54 -0700

    Fix next config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
-/** @type {import('next').NextConfig} */
-
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withTM = require("next-transpile-modules")(["three"]);
-
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
+  output: "standalone",
   reactStrictMode: true,
 };
 
