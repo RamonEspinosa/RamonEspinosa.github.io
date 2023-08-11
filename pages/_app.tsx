2ee8568692527b169ed78562f8be104528b35388
Juan Lopez <espinosalopezjuanramon@gmail.com>
2023-08-11 00:52:45 -0700

    Switch to nextjs fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,21 @@
 import React from "react";
 import { AppProps } from "next/app";
+import { Inter } from "next/font/google";
 import "styles/global.scss";
 
+const inter = Inter({ subsets: ["latin"] });
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${inter.style.fontFamily}, -apple-system,
+            BlinkMacSystemFont, avenir next, avenir, segoe ui, helvetica neue,
+            helvetica, Cantarell, Ubuntu, roboto, noto, arial, sans-serif;
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  );
 }
