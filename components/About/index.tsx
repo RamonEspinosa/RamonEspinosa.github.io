a0c2b37f1cec3a8d7b680046008e363df11b8dd9
Juan Lopez <espinosalopezjuanramon@gmail.com>
2023-08-14 00:42:26 -0700

    Shorten biography text

--- components/About/index.tsx
+++ components/About/index.tsx
@@ -1,71 +1,53 @@
 import React from "react";
+import styles from "./styles.module.scss";
 
 const About = () => {
   return (
-    <section
-      style={{
-        maxWidth: "72ch",
-        display: "flex",
-        gap: "16px",
-        flexDirection: "column",
-      }}
-    >
-      <h2>Get to know me better</h2>
-      <p>
-        TL;DR I am a <span>computer systems engineer</span> with over
-        <span> 5 years of working experience</span>.
-      </p>
-      <p>
-        I was first introduced to a computer when my brother taught me how to
-        play Age of Empires 2 in our first PC, a Compaq Presario S5100NX. To
-        this day, we still love playing that and other RTS games.
-      </p>
-      <p>
-        Since access to internet was extremely restricted and because there were
-        no libraries near my house, I relied on Microsoft Encarta to do my
-        homework. But at times I would get bored, which led me to discovering
-        other software bundled in Windows XP like Minesweeper or 3D Pinball
-        Space Cadet.
-      </p>
-      <p>
-        At the age of 15, I took courses on support and maintenance of computer
-        equipment. That paid itself as it got me some gigs and extra money,
-        which I then used to assemble my own PC.
-      </p>
-      <p>
-        I was so happy that I decided I should take this hobby of mine and
-        profit the hell out of it. So 2 years later, I enrolled into university
-        to study an engineering in Computer Systems. Of course it was not easy,
-        but with the support of my family I ended up graduating with honors.
-      </p>
-      <p>
-        I got my first professional job at a local company as a full-stack
-        developer, and even though I was nailing it, it caused me so much stress
-        due to the amount of work I got assigned and extremely tight deadlines.
-        I lasted a full year there until I decided to move on in 2019.
-      </p>
-      <p>
-        That is how I ended up in Prixz, where I met other developers like Hugo
-        and Andres who straightened up my career and taught me a lot of what I
-        know today.
-      </p>
-      <p>
-        Then I got a call from DaCodes, they were looking to hire a developer
-        that could in turn collaborate with TTT studios. At first, I was a bit
-        hesitant about leaving my team, but since the offer was very attractive,
-        I accepted.
-      </p>
-      <p>
-        90% of the time I was helping TTT studios with their own projects, and
-        they were so happy with my performance that in 2021 I got a job offer
-        from them. This time, I did not hesitate as I was already spending more
-        time with the folks at TTT.
-      </p>
-      <p>
-        And that is how I migrated to Canada where I first met (physically)
-        David, Chris and Gokul, whom I appreciate a lot by all the support they
-        have showed me.
-      </p>
+    <section className={styles.about__section}>
+      <article className={styles.about__biography}>
+        <h2>Get to know me better</h2>
+        <p>
+          TL;DR: Began with Age of Empires 2, now a computer systems engineer.
+          My journey involved tech courses, jobs, and collaborations, yielding
+          5+ years of experience.
+        </p>
+        <p>
+          My journey started when my brother taught me how to play Age of
+          Empires 2 on our first PC, a Compaq Presario S5100NX. Even today, that
+          game holds a special place in our hearts.
+        </p>
+        <p>
+          With limited internet access at the time, I turned to Microsoft
+          Encarta for my homework needs. Occasionally, I&apos;d become bored,
+          prompting me to explore other software like Minesweeper or 3D Pinball
+          Space Cadet. I was really fascinated by how much fun computers could
+          be.
+        </p>
+        <p>
+          In my teenage years, I took courses on technical computer assistance.
+          This earned me some gigs and money, which I then used to assemble my
+          own PC. The joy of profiting from my hobby inspired me to pursue
+          computer systems engineering. With the support of my family and
+          friends, I graduated with honors.
+        </p>
+        <p>
+          I got my first full-time job at a local company as a full-stack
+          developer. However, the stress drove me to seek new horizons after a
+          year. And that&apos;s how I ended up at Prixz, where I met other
+          developers like Hugo and Andres who shaped my career and taught me a
+          lot of what I know today.
+        </p>
+        <p>
+          After a while, DaCodes got in touch; they were looking for a developer
+          who could collaborate with TTT Studios. And in 2021, my performance at
+          TTT impressed them enough to result in a full-time job offer.
+        </p>
+        <p>
+          This is how I migrated to Canada, where I first met (physically)
+          David, Chris, and Gokul whom I greatly appreciate for all the support
+          and mentorship they have given me.
+        </p>
+      </article>
     </section>
   );
 };
