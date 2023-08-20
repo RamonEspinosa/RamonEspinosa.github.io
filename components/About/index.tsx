0e01387fc3ded16e4589c82fd358000fae69585f
Juan Lopez <espinosalopezjuanramon@gmail.com>
2023-08-19 21:43:24 -0700

    Add popovers in about me section

--- components/About/index.tsx
+++ components/About/index.tsx
@@ -1,47 +1,144 @@
+import {
+  Popover,
+  PopoverClose,
+  PopoverContent,
+  PopoverDescription,
+  PopoverHeading,
+  PopoverTrigger,
+} from "components/Popover";
+import Link from "next/link";
 import React from "react";
 import styles from "./styles.module.scss";
+
+const CustomPopover = ({
+  children,
+  message,
+  heading,
+}: {
+  children: React.ReactNode;
+  message: React.ReactNode;
+  heading: React.ReactNode;
+}) => {
+  return (
+    <Popover
+      arrowProps={{
+        className: styles["about__popover-arrow"],
+      }}
+      placement="top"
+    >
+      <PopoverTrigger className={styles["about__popover-trigger"]}>
+        {children}
+      </PopoverTrigger>
+      <PopoverContent className={styles["about__popover-content"]}>
+        <PopoverHeading className={styles["about__popover-heading"]}>
+          {heading}
+        </PopoverHeading>
+        <PopoverDescription className={styles["about__popover-description"]}>
+          {message}
+        </PopoverDescription>
+        <PopoverClose className="visually-hidden">Close</PopoverClose>
+      </PopoverContent>
+    </Popover>
+  );
+};
+
+const wizards = [
+  {
+    href: "https://github.com/HugoLiconV",
+    name: "Hugo Licon",
+  },
+  {
+    href: "https://github.com/Manuel-Espinosa",
+    name: "Manuel Espinosa",
+  },
+  {
+    href: "https://github.com/eduardo-rdguez",
+    name: "Eduardo Rodriguez",
+  },
+];
 
 const About = () => {
   return (
     <section className={styles.about__section}>
       <article className={styles.about__biography}>
         <h2>Get to know me better</h2>
-
         <p>
-          My journey started at 8, when my brother taught me how to play Age of
-          Empires 2 on our first PC, a Compaq Presario S5100NX. Even today, that
-          game holds a special place in our hearts.
+          I first interacted with a computer at 8, when my brother taught me how
+          to play{" "}
+          <CustomPopover
+            heading="Wololo!"
+            message={
+              <span>
+                Everytime the game was launched, this{" "}
+                <Link
+                  href="https://www.youtube.com/watch?v=_rS_n3JVTPE"
+                  target="_blank"
+                >
+                  intro video
+                </Link>{" "}
+                would start playing. Watching it now makes me so nostalgic.
+              </span>
+            }
+          >
+            Age of Empires 2
+          </CustomPopover>{" "}
+          on our first PC, a Compaq Presario S5100NX. Even today, that game
+          holds a special place in our hearts.
         </p>
         <p>
           With limited internet access at the time, I turned to Microsoft
           Encarta for my homework needs. Occasionally, I&apos;d become bored,
-          prompting me to explore other software like Minesweeper or 3D Pinball
-          Space Cadet. I was really fascinated by how much fun computers could
-          be.
+          prompting me to explore other software like Minesweeper or{" "}
+          <CustomPopover
+            heading="So many memories!"
+            message={
+              <span>
+                I feel someone should port this game to the web. It was really
+                fun to play back when there was no internet. Feel free to watch{" "}
+                <Link
+                  href="https://www.youtube.com/watch?v=uMZwb05pPnI"
+                  target="_blank"
+                >
+                  video gameplay here
+                </Link>
+                .
+              </span>
+            }
+          >
+            3D Pinball Space Cadet
+          </CustomPopover>
+          . I was really fascinated by how much fun computers could be.
         </p>
         <p>
-          In my teenage years, I took courses on technical computer assistance.
-          This earned me some gigs and money, which I then used to assemble my
-          own PC. The joy of profiting from my hobby inspired me to pursue
-          computer systems engineering. With the support of my family and
-          friends, I graduated with honors.
+          Some time later, in my teenage years, I took courses on technical
+          computer assistance. This earned me some gigs and money, which I then
+          used to assemble my own PC. The joy of profiting from my hobby
+          inspired me to pursue computer systems engineering. With the support
+          of my family and friends, I graduated with honors.
         </p>
         <p>
-          Then, I got my first full-time job at a local company as a full-stack
-          developer. However, the stress drove me to seek new horizons after a
-          year. And that&apos;s how I ended up at Prixz, where I met other
-          developers like Hugo and Andres who shaped my career and taught me a
-          lot of what I know today.
-        </p>
-        <p>
-          After a while, DaCodes got in touch; they were looking for a developer
-          who could collaborate with TTT Studios. And in 2021, my performance at
-          TTT impressed them enough to result in a full-time job offer.
-        </p>
-        <p>
-          This is how I migrated to Canada, where I first met (physically)
-          David, Chris, and Gokul whom I greatly appreciate for all the support
-          and mentorship they have given me.
+          Since then, I&apos;ve had multiple jobs where I&apos;ve met{" "}
+          <CustomPopover
+            heading="Software wizards"
+            message={
+              <span>
+                Check out this awesome crew of devs I really respect.
+                They&apos;ve got their own unique skills, but one thing they all
+                get is that coding is a human process.
+                <ul className={styles.about__wizards}>
+                  {wizards.map((wizard) => (
+                    <li key={wizard.name}>
+                      <Link href={wizard.href}>{wizard.name}</Link>
+                    </li>
+                  ))}
+                </ul>
+              </span>
+            }
+          >
+            other passionate developers
+          </CustomPopover>
+          , many of whom have shaped my career and taught me lots of the things
+          I know today.
         </p>
         <p>
           <span className={styles.about__tldr}>TL;DR:</span> Began with Age of
---
+++ components/Popover/index.tsx
@@ -0,0 +1,283 @@
+import * as React from "react";
+import {
+  useFloating,
+  autoUpdate,
+  offset,
+  flip,
+  shift,
+  useClick,
+  useDismiss,
+  useRole,
+  useInteractions,
+  useMergeRefs,
+  Placement,
+  FloatingPortal,
+  FloatingFocusManager,
+  useId,
+  useHover,
+  safePolygon,
+  arrow,
+  FloatingArrow,
+  FloatingArrowProps,
+} from "@floating-ui/react";
+
+const ARROW_HEIGHT = 7;
+const ARROW_GAP = 3;
+
+interface PopoverOptions {
+  initialOpen?: boolean;
+  placement?: Placement;
+  modal?: boolean;
+  open?: boolean;
+  onOpenChange?: (open: boolean) => void;
+  allowHover?: boolean;
+  allowClick?: boolean;
+  arrowRef?: React.MutableRefObject<SVGSVGElement>;
+  arrowProps?: Partial<FloatingArrowProps>;
+}
+
+export function usePopover({
+  initialOpen = false,
+  placement = "top",
+  modal,
+  open: controlledOpen,
+  onOpenChange: setControlledOpen,
+  allowHover = true,
+  allowClick = true,
+  arrowRef,
+  arrowProps,
+}: PopoverOptions = {}) {
+  const [uncontrolledOpen, setUncontrolledOpen] = React.useState(initialOpen);
+  const [labelId, setLabelId] = React.useState<string | undefined>();
+  const [descriptionId, setDescriptionId] = React.useState<
+    string | undefined
+  >();
+
+  const open = controlledOpen ?? uncontrolledOpen;
+  const setOpen = setControlledOpen ?? setUncontrolledOpen;
+  const data = useFloating({
+    placement,
+    open,
+    onOpenChange: setOpen,
+    whileElementsMounted: autoUpdate,
+    middleware: [
+      offset(ARROW_HEIGHT + ARROW_GAP),
+      flip({
+        crossAxis: placement.includes("-"),
+        fallbackAxisSideDirection: "end",
+        padding: 5,
+      }),
+      shift({ padding: 5 }),
+      arrow({ element: arrowRef }),
+    ],
+  });
+
+  const context = data.context;
+
+  const hover = useHover(context, {
+    enabled: controlledOpen == null,
+    handleClose: safePolygon(),
+  });
+  const click = useClick(context, {
+    enabled: controlledOpen == null,
+  });
+
+  const dismiss = useDismiss(context);
+  const role = useRole(context);
+
+  const interactions = useInteractions(
+    [allowHover && hover, allowClick && click, dismiss, role].filter(Boolean)
+  );
+
+  return React.useMemo(
+    () => ({
+      open,
+      setOpen,
+      ...interactions,
+      ...data,
+      modal,
+      labelId,
+      descriptionId,
+      setLabelId,
+      setDescriptionId,
+      arrowRef,
+      arrowProps,
+    }),
+    [
+      open,
+      setOpen,
+      interactions,
+      data,
+      modal,
+      labelId,
+      descriptionId,
+      arrowRef,
+      arrowProps,
+    ]
+  );
+}
+
+type ContextType =
+  | (ReturnType<typeof usePopover> & {
+      setLabelId: React.Dispatch<React.SetStateAction<string | undefined>>;
+      setDescriptionId: React.Dispatch<
+        React.SetStateAction<string | undefined>
+      >;
+    })
+  | null;
+
+const PopoverContext = React.createContext<ContextType>(null);
+
+export const usePopoverContext = () => {
+  const context = React.useContext(PopoverContext);
+
+  if (context == null) {
+    throw new Error("Popover components must be wrapped in <Popover />");
+  }
+
+  return context;
+};
+
+export function Popover({
+  children,
+  modal = false,
+  ...restOptions
+}: {
+  children: React.ReactNode;
+} & PopoverOptions) {
+  // This can accept any props as options, e.g. `placement`,
+  // or other positioning options.
+  const arrowRef = React.useRef(null);
+  const popover = usePopover({ modal, arrowRef, ...restOptions });
+  return (
+    <PopoverContext.Provider value={popover}>
+      {children}
+    </PopoverContext.Provider>
+  );
+}
+
+interface PopoverTriggerProps {
+  children: React.ReactNode;
+  asChild?: boolean;
+}
+
+export const PopoverTrigger = React.forwardRef<
+  HTMLElement,
+  React.HTMLProps<HTMLElement> & PopoverTriggerProps
+>(function PopoverTrigger({ children, asChild = false, ...props }, propRef) {
+  const context = usePopoverContext();
+  const childrenRef = (children as any).ref;
+  const ref = useMergeRefs([context.refs.setReference, propRef, childrenRef]);
+
+  // `asChild` allows the user to pass any element as the anchor
+  if (asChild && React.isValidElement(children)) {
+    return React.cloneElement(
+      children,
+      context.getReferenceProps({
+        ref,
+        ...props,
+        ...children.props,
+        "data-state": context.open ? "open" : "closed",
+      })
+    );
+  }
+
+  return (
+    <button
+      ref={ref}
+      type="button"
+      // The user can style the trigger based on the state
+      data-state={context.open ? "open" : "closed"}
+      {...context.getReferenceProps(props)}
+    >
+      {children}
+    </button>
+  );
+});
+
+export const PopoverContent = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLProps<HTMLDivElement>
+>(function PopoverContent({ style, ...props }, propRef) {
+  const { context: floatingContext, ...context } = usePopoverContext();
+  const ref = useMergeRefs([context.refs.setFloating, propRef]);
+
+  if (!floatingContext.open) return null;
+
+  return (
+    <FloatingPortal>
+      <FloatingFocusManager context={floatingContext} modal={context.modal}>
+        <div
+          ref={ref}
+          style={{ ...context.floatingStyles, ...style }}
+          aria-labelledby={context.labelId}
+          aria-describedby={context.descriptionId}
+          {...context.getFloatingProps({ ...props })}
+        >
+          {props.children}
+          <FloatingArrow
+            {...context.arrowProps}
+            context={floatingContext}
+            ref={context.arrowRef}
+          />
+        </div>
+      </FloatingFocusManager>
+    </FloatingPortal>
+  );
+});
+
+export const PopoverHeading = React.forwardRef<
+  HTMLHeadingElement,
+  React.HTMLProps<HTMLHeadingElement>
+>(function PopoverHeading(props, ref) {
+  const { setLabelId } = usePopoverContext();
+  const id = useId();
+
+  // Only sets `aria-labelledby` on the Popover root element
+  // if this component is mounted inside it.
+  React.useLayoutEffect(() => {
+    setLabelId(id);
+    return () => setLabelId(undefined);
+  }, [id, setLabelId]);
+
+  return (
+    <h2 {...props} ref={ref} id={id}>
+      {props.children}
+    </h2>
+  );
+});
+
+export const PopoverDescription = React.forwardRef<
+  HTMLParagraphElement,
+  React.HTMLProps<HTMLParagraphElement>
+>(function PopoverDescription(props, ref) {
+  const { setDescriptionId } = usePopoverContext();
+  const id = useId();
+
+  // Only sets `aria-describedby` on the Popover root element
+  // if this component is mounted inside it.
+  React.useLayoutEffect(() => {
+    setDescriptionId(id);
+    return () => setDescriptionId(undefined);
+  }, [id, setDescriptionId]);
+
+  return <p {...props} ref={ref} id={id} />;
+});
+
+export const PopoverClose = React.forwardRef<
+  HTMLButtonElement,
+  React.ButtonHTMLAttributes<HTMLButtonElement>
+>(function PopoverClose(props, ref) {
+  const { setOpen } = usePopoverContext();
+  return (
+    <button
+      type="button"
+      ref={ref}
+      {...props}
+      onClick={(event) => {
+        props.onClick?.(event);
+        setOpen(false);
+      }}
+    />
+  );
+});
