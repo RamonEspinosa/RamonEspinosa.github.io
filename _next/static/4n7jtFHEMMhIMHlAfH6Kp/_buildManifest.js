ab9e8aa95026cdb14b43a13198b26fcd60317774
RamonEspinosa <RamonEspinosa@users.noreply.github.com>
2023-08-24 08:12:49 +0000

    deploy: 68cb085bbaa771ee5e1024e25932357d4c169f6d

--- _next/static/4n7jtFHEMMhIMHlAfH6Kp/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/931-2ee13be6f1b0d5be.js","static/css/6db0c6f20ff9ba7c.css","static/chunks/pages/index-0d92ef230867a613.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
