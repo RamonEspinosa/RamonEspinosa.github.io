312a745986d1a2f7ee54a84b55379be6f5b77053
Juan Lopez <espinosalopezjuanramon@gmail.com>
2023-08-26 15:39:09 -0700

    Remove AnimatePresence in popover

--- components/Popover/index.tsx
+++ components/Popover/index.tsx
@@ -20,7 +20,7 @@ import {
   FloatingArrow,
   FloatingArrowProps,
 } from "@floating-ui/react";
-import { AnimatePresence, m } from "framer-motion";
+import { m } from "framer-motion";
 
 const ARROW_HEIGHT = 7;
 const ARROW_GAP = 2;
@@ -208,26 +208,24 @@ export const PopoverContent = React.forwardRef<
   return (
     <FloatingPortal>
       <FloatingFocusManager context={floatingContext} modal={context.modal}>
-        <AnimatePresence>
-          <m.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            transition={{ duration: 0.3 }}
-            ref={ref}
-            style={{ ...context.floatingStyles, ...style }}
-            aria-labelledby={context.labelId}
-            aria-describedby={context.descriptionId}
-            {...context.getFloatingProps({ ...props })}
-          >
-            {props.children}
-            <FloatingArrow
-              {...context.arrowProps}
-              context={floatingContext}
-              ref={context.arrowRef}
-            />
-          </m.div>
-        </AnimatePresence>
+        <m.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.3 }}
+          ref={ref}
+          style={{ ...context.floatingStyles, ...style }}
+          aria-labelledby={context.labelId}
+          aria-describedby={context.descriptionId}
+          {...context.getFloatingProps({ ...props })}
+        >
+          {props.children}
+          <FloatingArrow
+            {...context.arrowProps}
+            context={floatingContext}
+            ref={context.arrowRef}
+          />
+        </m.div>
       </FloatingFocusManager>
     </FloatingPortal>
   );
