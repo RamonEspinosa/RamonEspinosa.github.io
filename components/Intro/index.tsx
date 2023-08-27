3d2ab14d7126bee9bb7d0654a456888e74d75111
Juan Lopez <espinosalopezjuanramon@gmail.com>
2023-08-26 20:53:07 -0700

    Add animation to picture

--- components/Intro/index.tsx
+++ components/Intro/index.tsx
@@ -4,8 +4,10 @@ import Contact from "components/Contact";
 import Image from "next/image";
 import styles from "./styles.module.scss";
 import profilePic from "public/images/me.webp";
+import { useAnimate, m } from "framer-motion";
 
 const Intro = () => {
+  const [scope, animate] = useAnimate();
   return (
     <section className={styles.intro__section}>
       <div className={styles.intro__wrapper}>
@@ -13,15 +15,48 @@ const Intro = () => {
           <Welcome />
           <Contact />
         </div>
-        <div className={styles.intro__frame}>
+        <m.div
+          initial={{
+            scale: 0,
+            opacity: 0,
+          }}
+          ref={scope}
+          className={styles.intro__frame}
+        >
           <Image
             className={styles.intro__picture}
             src={profilePic}
             alt="Juan Lopez headshot"
             priority
             sizes="(min-width: 550px) 90vw,  400px"
+            onLoad={() => {
+              animate([
+                [
+                  scope.current,
+                  {
+                    opacity: 1,
+                  },
+                  {
+                    duration: 0.5,
+                  },
+                ],
+                [
+                  scope.current,
+                  {
+                    scale: 1,
+                  },
+                  {
+                    type: "spring",
+                    bounce: 0.25,
+                    duration: 0.5,
+                    damping: 16,
+                    at: "<",
+                  },
+                ],
+              ]);
+            }}
           />
-        </div>
+        </m.div>
       </div>
     </section>
   );
