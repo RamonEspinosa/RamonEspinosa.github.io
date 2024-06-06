2cc54fc9fbd753ec32b3ca1c374242fe0027a999
RamonEspinosa <RamonEspinosa@users.noreply.github.com>
2024-06-06 22:14:22 +0000

    deploy: 4c28641b3dc49cc6b19182f3e413f9d80e8101ec

---
+++ _next/static/YjsoaHwTpk2k_4rRw0OVo/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/884-17e431e93047f63e.js","static/css/ae26d1b68bf8d98f.css","static/chunks/pages/index-f4e5ddff16a46de1.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
